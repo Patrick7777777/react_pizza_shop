a01eca515442ca27cb53c9a14765f36b224560d9
Kireev Eygeniy <ekireev@live.com>
2020-10-11 23:34:14 +0300

    install react-redux

--- src/index.js
+++ src/index.js
@@ -3,29 +3,6 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
 import './scss/app.scss';
 import App from './App';
-import { createStore } from "redux";
-
-function counter (state = 0, action) {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1
-        case 'DECREMENT':
-            return state - 1
-        default:
-            return state
-
-    }
-}
-
-const store = createStore(counter);
-
-store.subscribe(()=>console.log('хранилище изменилось', store.getState()));
-
-console.log(store.getState());
-
-store.dispatch({type: 'INCREMENT'});
-store.dispatch({type: 'INCREMENT'});
-
 
 ReactDOM.render(
 
