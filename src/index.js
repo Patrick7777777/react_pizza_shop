1e279cd111ed0ccd6e35654e38b4f9f9e99d3059
Kireev Eygeniy <ekireev@live.com>
2020-10-13 10:33:11 +0300

    lesson #6 - create actions & reducers, install rudux devtools

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
 import './scss/app.scss';
 import App from './App';
+import Provider from "react-redux/lib/components/Provider";
+import store from "./redux/store";
+
+store.dispatch({
+    type: 'SET_SORT_BY',
+    payload: 'price'
+});
 
 ReactDOM.render(
 
       <Router>
-          <App />
+          <Provider store={store}>
+              <App />
+          </Provider>
       </Router>,
 
   document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,9 @@
 import {createStore} from "redux";
+import rootReducer from "./reducers";
 
-
-function counter(state = 0, action) {
-    switch (action.type) {
-        case 'ДОБАВИТЬ':
-            return state + 1;
-        case 'ОТНЯТЬ':
-            return state - 1;
-        default:
-            return state
-    }
-}
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 
-const store = createStore(counter);
 
 export default store;
